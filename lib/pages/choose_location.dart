78461e7d5a105f2902b0e2cc0a05e4ac8137d762
Oladimeji Omotosho <oladimejiomotosho5@gmail.com>
2021-07-21 22:44:47 +0100

    pages new

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -6,10 +6,33 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  int counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    print('initState function ran');
+  }
+
   @override
   Widget build(BuildContext context) {
+    print('build function ran');
     return Scaffold(
-      
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text("Choose a Location"),
+        elevation: 0,
+        centerTitle: true,
+      ),
+      body: RaisedButton(
+        onPressed: () {
+          setState(() {
+            counter += 1;
+          });
+        },
+        child: Text('counter is $counter'),
+      ),
     );
   }
 }
